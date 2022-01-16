959fea5ab0de81bcefccf90d0fc5c4c9dc9f719c
bgrosman <64140894+bgrosman@users.noreply.github.com>
2022-01-16 14:28:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -14,23 +14,26 @@ productsPrices.set(3, "4000");
 productsPrices.set(4, "1500");
 productsPrices.set(5, "1000");
 
+let currentOrderDetails = [];
+
 function initOrdersForm() {
-  document.getElementById("create-order-button").innerHTML = "Create Order";
+  document.getElementById("create-close-order-button").innerHTML = "Create Order";
   document.getElementById("form-select-customer").selectedIndex = 0;
   document.getElementById("form-text-customer=address").value = "";
   document.getElementById("form-select-product").selectedIndex = 0;
   document.getElementById("form-text-price").value = "";
   document.getElementById("form-text-comment").value = "";
+  document.getElementById("create-update-button").innerHTML = "Create";
 }
 
 function createOrderClick() {
   let ordersForm = document.getElementById("orders-form");
-  let createOrderButton = document.getElementById("create-order-button");
+  let createCloseOrderButton = document.getElementById("create-close-order-button");
   if (ordersForm && ordersForm.style) {
     ordersForm.style.display = ordersForm.style.display != "none" ? "none" : "";
   }
-  if (createOrderButton.innerHTML == "Create Order") {
-    createOrderButton.innerHTML = "Close Order";
+  if (createCloseOrderButton.innerHTML == "Create Order") {
+    createCloseOrderButton.innerHTML = "Close Order";
   } else {
     initOrdersForm();
   }
@@ -56,7 +59,7 @@ function createButtonClick() {
   let formTextComment = document.getElementById("form-text-comment");
   let ordersTable = document.getElementById("orders-table");
   let ordersForm = document.getElementById("orders-form");
-  let createOrderButton = document.getElementById("create-order-button");
+  let createUpdateButton = document.getElementById("create-update-button");
   if (formSelectCustomer.selectedIndex == 0) {
     alert("Please choose cutomer");
   } else if (formTextAddress.value == "") {
@@ -68,30 +71,58 @@ function createButtonClick() {
   } else if (formTextPrice.value.match(/^[0-9]+$/) == null) {
     alert("Price should be a number");
   } else {
-    let tableLength = ordersTable.rows.length;
-    let row = ordersTable.insertRow(tableLength);
-    if (tableLength % 2 == 0) {
-      row.classList.add("table-primary");
+    if (createUpdateButton.innerHTML == "Create") {
+      let tableLength = ordersTable.rows.length;
+      let row = ordersTable.insertRow(tableLength);
+      if (tableLength % 2 == 0) {
+        row.classList.add("table-primary");
+      } else {
+        row.classList.add("table-secondary");
+      }
+      let orderIdCell = row.insertCell(0);
+      let customerNameCell = row.insertCell(1);
+      let customerAddressCell = row.insertCell(2);
+      let productCell = row.insertCell(3);
+      let priceCell = row.insertCell(4);
+      let commentCell = row.insertCell(5);
+      row.addEventListener("click", rowClick, false);
+      orderIdCell.outerHTML = "<th scope='row'>" + tableLength + "</th>";
+      customerNameCell.innerHTML = formSelectCustomer.options[formSelectCustomer.selectedIndex].text;
+      customerAddressCell.innerHTML = formTextAddress.value;
+      productCell.innerHTML = formSelectProduct.options[formSelectProduct.selectedIndex].text;
+      priceCell.innerHTML = formTextPrice.value;
+      commentCell.innerHTML = formTextComment.value;
+      alert("Order added successfully!");
     } else {
-      row.classList.add("table-secondary");
+      currentOrderDetails[1].innerHTML = formSelectCustomer.options[formSelectCustomer.selectedIndex].text;
+      currentOrderDetails[2].innerHTML = formTextAddress.value;
+      currentOrderDetails[3].innerHTML = formSelectProduct.options[formSelectProduct.selectedIndex].text;
+      currentOrderDetails[4].innerHTML = formTextPrice.value;
+      currentOrderDetails[5].innerHTML = formTextComment.value;
+      alert("Order number " +  currentOrderDetails[0].textContent + " updated successfully!");
     }
-    let orderIdCell = row.insertCell(0);
-    let customerNameCell = row.insertCell(1);
-    let customerAddressCell = row.insertCell(2);
-    let productCell = row.insertCell(3);
-    let priceCell = row.insertCell(4);
-    let commentCell = row.insertCell(5);
-    for (let i = 1; i < row.cells.length; i++) {
-      row.cells[i].setAttribute("contenteditable", "true");
-    }
-    orderIdCell.outerHTML = "<th scope='row'>" + tableLength + "</th>";
-    customerNameCell.innerHTML = formSelectCustomer.options[formSelectCustomer.selectedIndex].text;
-    customerAddressCell.innerHTML = formTextAddress.value;
-    productCell.innerHTML = formSelectProduct.options[formSelectProduct.selectedIndex].text;
-    priceCell.innerHTML = formTextPrice.value;
-    commentCell.innerHTML = formTextComment.value;
     ordersForm.style.display = "none";
     initOrdersForm();
-    alert("Order added successfully!");
   }
+}
+
+function rowClick() {
+  currentOrderDetails = [];
+  for (let i = 0; i < this.cells.length; i++) {
+    currentOrderDetails.push(this.cells[i]);
+  }
+  let formSelectCustomer = document.getElementById("form-select-customer");
+  let formTextAddress = document.getElementById("form-text-customer=address");
+  let formSelectProduct = document.getElementById("form-select-product");
+  let formTextPrice = document.getElementById("form-text-price");
+  let formTextComment = document.getElementById("form-text-comment");
+  let ordersForm = document.getElementById("orders-form");
+  ordersForm.style.display = "";
+  formSelectCustomer.selectedIndex = [...formSelectCustomer.options].findIndex (option => option.text == currentOrderDetails[1].textContent);
+  formTextAddress.value = currentOrderDetails[2].textContent;
+  formSelectProduct.selectedIndex = [...formSelectProduct.options].findIndex (option => option.text == currentOrderDetails[3].textContent);
+  formTextPrice.value = currentOrderDetails[4].textContent;
+  formTextComment.value = currentOrderDetails[5].textContent;
+  document.getElementById("create-update-button").innerHTML = "Update";
+  document.getElementById("create-close-order-button").innerHTML = "Close Order";
 }
